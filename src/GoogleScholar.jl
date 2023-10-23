2be074777f3c50764b9c3f11d56cab9c37fd5632
Robert Moss <rmoss92@gmail.com>
2023-10-23 11:18:35 -0700

    Added exports

--- src/GoogleScholar.jl
+++ src/GoogleScholar.jl
@@ -3,6 +3,12 @@ module GoogleScholar
 using HTTP
 using Plots
 
+export
+    Scholar,
+    get_citation_history!,
+    plot_citations,
+    bettersavefig
+
 include("scholar.jl")
 include("plots.jl")
 
